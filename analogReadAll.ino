390227bb15eead220fd40f77fa93795fdc0e23fa
jake waters <jerkey@g-cipher.net>
2019-02-22 20:10:32 -0800

    read and display 16 analogs once per second

--- analogReadAll.ino
+++ analogReadAll.ino
@@ -1,19 +1,27 @@
+#define INTERVAL        1000 // how many milliseconds between print events
+
+unsigned long adders[16]; // store added up values
+float addcount;
+unsigned long lastPrintTime;
+
 void setup() {
   Serial.begin(57600);
-  Serial.println("booted up, analogReadAll.ino printing A1 A2 A3 A4 A5 in hex");
-  pinMode(2,OUTPUT);
+  Serial.println("booted up, analogReadAll.ino printing A1-A15");
+  lastPrintTime = millis();
 }
 
 void loop() {
-  Serial.print(analogRead(A1),HEX);
-  Serial.print("  ");
-  Serial.print(analogRead(A2),HEX);
-  Serial.print("  ");
-  Serial.print(analogRead(A3),HEX);
-  Serial.print("  ");
-  Serial.print(analogRead(A4),HEX);
-  Serial.print("  ");
-  Serial.print(analogRead(A5),HEX);
-  Serial.println("  ");
-  delay(200);
+  for (int i=0; i<16; i++) adders[i] = 0;
+  addcount = 0;
+  while (millis() - lastPrintTime < INTERVAL) {
+    for (int i=0; i<16; i++) adders[i] += analogRead(A0 + i);
+    addcount += 1;
+  }
+  lastPrintTime = millis();
+  for (int i=0; i<16; i++) {
+    Serial.print((float)adders[i]/addcount,1);
+    Serial.print("\t"); // tab
+  }
+  Serial.print(millis() / 1000);
+  Serial.print("\n"); // newline
 }
